cddd79c8008c8431be812d945013ce8c197cd3db
IrfanHidayatulahP <irfanhputra400@gmail.com>
2023-06-22 15:42:38 +0700

    membuat display menu

--- CircularQueues_142/CircularQueues_142.cpp
+++ CircularQueues_142/CircularQueues_142.cpp
@@ -94,13 +94,27 @@ public:
 				cout << queue_array[FRONT_position] << "	";
 				FRONT_position++;
 			}
+			cout << endl;
 		}
 	}
 };
 
 
-
-
 int main() {
+	Queues q;
+	char ch;
 
+	while (true) {
+		try {
+			cout << "Menu" << endl;
+			cout << "1. Implement insert operation" << endl;
+			cout << "2. Implement delete operation" << endl;
+			cout << "3. Display values" << endl;
+			cout << "4. Exit" << endl;
+			cout << "Enter your choice (1-4): " << endl;
+			cin >> ch;
+			cout << endl;
+
+		}
+	}
 }
